74b74d023f91e141ee135030eac76912baaa2f0c
Stas Ukolov <ukoloff@gmail.com>
2022-04-08 14:06:15 +0500

    Store visualisation

--- samples/tesaption/script/view.py
+++ samples/tesaption/script/view.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path
 
 root = Path(__file__).parents[1]
@@ -11,10 +12,14 @@ dst.mkdir(exist_ok=True)
 for f in src.glob('*.pcglns'):
   name = f.stem
   print(name, end=' ', flush=True)
+  res = f.with_name(name + '.result.txt')
+  out = dst / f'{name}.html'
   os.system(f"""{bat} {
-    f.with_name(name + '.result.txt')} {
+    res} {
     json / f'{name}.dbs.json'} {
     json / f'{name}.task.json'} -fo {
-    dst / f'{name}.html'} > {
+    out} > {
     dst / f'{name}.txt'}""")
-
+  result = open(res).readlines()
+  result[-1:-1] = [line for line in open(out) if re.match(r'Route\s+Length\s*:', line)]
+  open(res, 'w').writelines(result)
